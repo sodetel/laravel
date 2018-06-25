8fc12c50b739912621d2ef29abaf658760afb296
Ahmad Moussawi <aymoussawi@gmail.com>
2018-06-25 19:05:19 +0300

    database: raw statements

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,67 @@ Route::get('/output/view', function () {
 
 });
 
+
+Route::get('/plans', function () {
+
+    $plans = DB::select('select * from plans');
+
+    return $plans;
+
+});
+
+Route::get('/plans/{id}', function ($id) {
+
+    $plan = DB::select('select * from plans where id = ? limit 1', [ $id ]);
+
+    return $plan;
+
+});
+
+Route::post('/plans', function() {
+
+    // $plan = [
+    //     'name' => 'Super Ultimate',
+    //     'created_at' => date('Y-m-d'),
+    //     'updated_at' => date('Y-m-d'),
+    // ];
+
+    $name = request()->get('name');
+
+    $values = [$name , date('Y-m-d'), date('Y-m-d')];
+
+    DB::insert('insert into `plans`(name, created_at, updated_at) values(?, ?, ?)', $values);
+
+    return ['status' => 'ok'];
+
+});
+
+Route::delete('/plans', function () {
+
+    $deleted = DB::delete('delete from plans');
+
+    return [
+        'status' => 'ok',
+        'deleted' => $deleted
+    ];
+
+});
+
+Route::delete('/plans/{id}', function ($id) {
+
+    // $deleted = DB::delete('delete from plans where id = ?', [$id]);
+    // here we are using the named parameters, it helps when we have complex
+    // query with many parameters, so the order is not important
+    $deleted = DB::delete('delete from plans where id = :id', ['id' => $id]);
+
+    return [
+        'status' => 'ok',
+        'deleted' => $deleted
+    ];
+
+});
+
+Route::get('/mafia😎', function () {
+    DB::statement('create table mafia (id serial);');
+    return 'table created';
+});
